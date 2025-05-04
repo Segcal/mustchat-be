c2fafb3820a362868448d830f082434e362de8a9
segun <seguncaleb3@gmail.com>
2025-05-04 03:50:57 +0100

    second

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const databaseURL = process.env.DATABASE_URL;
 
 app.use(
     cors({
-        origin: process.env.ORIGIN,
+        origin: 'https://mustchat-fe.vercel.app',
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
